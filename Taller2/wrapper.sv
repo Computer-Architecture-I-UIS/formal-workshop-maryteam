2f37eba6de1e319df8173501dea1d640ba294a08
alejandro <you@example.com>
2020-07-01 22:31:19 -0500

    agregué assert para verificar que io_cont incremente en1

--- Taller2/wrapper.sv
+++ Taller2/wrapper.sv
@@ -14,18 +14,31 @@ reg init=1;
 always @(posedge clock) begin
 	if (init) begin
 		assume(reset);
-		assume(io_inc);
-		assume(io_duty==5);
-		assume(io_T==10);
+		//assume(io_inc);
+		//assume(io_duty<=255);
+		//assume(io_T<=255);
 	end
 	if (!reset) begin
-		if ($past(io_inc)) assert(!io_cont);
-		if ($past(io_inc,2)) assert(io_cont);
-		if (io_cont<= io_duty) assert(io_out);
-		if (io_cont> io_duty) assert(!io_out);
-		if (io_cont>=io_T) assert(!io_out);
-		if (reset) assert(!io_out);	
-	end     
+		if ($past(io_inc))begin
+			assert(!io_cont);
+		end
+		if ($past(io_inc,2))begin
+			assert(io_cont);
+		end
+		if (io_cont<= io_duty)begin
+			assert(io_out);
+		end
+		if (io_cont> io_duty)begin 
+			assert(!io_out);
+		end
+		if (io_cont<=io_T)begin
+			assert($past(io_cont)+1==io_cont);
+		end
+		//if (io_cont>=io_T)assert(!io_out);
+		assert(io_cont<=io_T);
+		//if (reset) assert(!io_out);
+		assert(io_duty<=io_T);	
+	end 
 	if(!io_inc)assert(!io_out);
 end 
 
